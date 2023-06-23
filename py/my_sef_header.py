fd4805e971de17e32bb50049d24a37bca8dc5200
Ben Denckla <bdenckla@alum.mit.edu>
2023-06-23 12:59:16 -0400

    update my_sef_header_.py

--- py/my_sef_header.py
+++ py/my_sef_header.py
@@ -8,6 +8,8 @@ def sef_header(bkid):
     return {
         'Index Title': my_sef_cmn.SEF_ENGLISH_BOOK_NAMES[bkid],
         'Version Title': 'Miqra according to the Masorah',
+        # XXX possibly add a version (e.g. v2021.12.07)
+        # to the Version Title string above
         'Language': 'he',
         'Version Source': '/'.join((
             'https://en.wikisource.org', 'wiki', 'User:Dovi',
