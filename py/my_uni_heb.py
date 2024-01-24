8d7bb352998cd76571279a1d9f538b48ba39e67b
Ben Denckla <bdenckla@alum.mit.edu>
2024-01-24 10:51:20 -0500

    reflect two meteg marks removed from Deut

--- py/my_uni_heb.py
+++ py/my_uni_heb.py
@@ -1,32 +1,17 @@
 """
 This module exports:
-    do_quick_test
     shunna
     accent_names
     hechar_names
-    write_verse_un
-    comma_shunna
+    comma_shunnas
     t_shunna
 """
 
 import unicodedata
-import my_html
-import my_tanakh_book_names as tbn
 import my_hebrew_letters as hl
 import my_hebrew_points as hpo
 import my_hebrew_accents as ha
 import my_str_defs as sd
-import my_render_wikitext as rwt
-
-
-def do_quick_test():
-    """ Do a quick test. """
-    lines1 = _get_pre_lines(' ', ' '+'אבג')
-    lines2 = _get_pre_lines(' ', 'דהו'+' ')
-    lines3 = _get_pre_lines(' ', ' '+'זחט'+' ')
-    assert lines1 == [' ', 'אבג']
-    assert lines2 == ['דהו', ' ']
-    assert lines3 == [' ', 'זחט', ' ']
 
 
 def shunna(string):
@@ -53,19 +38,6 @@ def hechar_names(string):
     return (_HE_TO_NONHE_DIC[c] for c in string)
 
 
-def write_verse_un(out_fp, bcvt, multiverse):
-    """ Write verse in "unicode names" format """
-    bkid, chnu, vrnu = tbn.bcvt_get_bcv_triple(bcvt)
-    vtrad = tbn.bcvt_get_vtrad(bcvt)
-    out_fp.write(f'{bkid} {chnu}:{vrnu} in vtrad {vtrad}\n')
-    for rv_cant, body in multiverse.items():
-        if len(multiverse) > 1:
-            _write_segments(out_fp, body, rv_cant)
-        else:
-            _write_segments(out_fp, body, None)
-    out_fp.write('\n')
-
-
 def comma_shunnas(string):
     """ Comma-joined shortened unicode names """
     return ','.join(t_shunnas(string))
@@ -87,69 +59,6 @@ def _mk_he_to_nonhe_dic():
 
 def _shorten_fullname_prefix(word1, word2):
     return _SHORTEN_DIC.get((word1, word2)) or word1 + ' ' + word2
-
-
-def _write_segments(out_fp, some_kind_of_verse, cant_dab=None, indent=''):
-    if isinstance(some_kind_of_verse, (list, tuple)):
-        html_els = some_kind_of_verse
-        _write_segments_from_html_els(
-            out_fp, html_els, cant_dab, indent)
-        return
-    if isinstance(some_kind_of_verse, rwt.VerseAndFriends):
-        veraf = some_kind_of_verse
-        for html_els in (veraf.verse, veraf.next_cp, veraf.good_ending):
-            _write_segments_from_html_els(
-                out_fp, html_els, cant_dab, indent)
-        return
-    assert False, some_kind_of_verse
-
-
-def _write_segments_from_html_els(out_fp, html_els, cant_dab=None, indent=''):
-    if cant_dab:
-        out_fp.write(f'{cant_dab}\n')
-        indent = '    '
-    for html_el in html_els:
-        if isinstance(html_el, str):
-            pre_lines = [html_el]
-            for sep in ' ', sd.NBSP:
-                list_of_lists = [_get_pre_lines(sep, pl) for pl in pre_lines]
-                pre_lines = sum(list_of_lists, [])
-            for pre_line in pre_lines:
-                line = comma_shunnas(pre_line)
-                out_fp.write(indent + line + '\n')
-            continue
-        if isinstance(html_el, dict):
-            segtag = my_html.hel_get_tag(html_el)  # e.g. 'span'
-            attr = html_el.get('attr')
-            kev_strs = _key_eq_val_strs(attr or {})
-            if contents := html_el.get('contents'):
-                out_fp.write(indent + _stasto('START', segtag, kev_strs))
-                _write_segments(out_fp, contents, None, indent)
-                out_fp.write(indent + _stasto('STOP', segtag, kev_strs))
-            else:
-                out_fp.write(indent + _stasto('START/STOP', segtag, kev_strs))
-            continue
-        assert False, 'instance of unexpected type'
-
-
-def _key_eq_val_strs(dic):
-    return tuple(f'{key}={val}' for key, val in dic.items())
-
-
-def _stasto(stasto, segtag, kev_strs):
-    return ' '.join((stasto, segtag, *kev_strs)) + '\n'
-
-
-def _get_pre_lines(sep, segment):
-    pre_lines = []
-    nsruns = segment.split(sep)  # nsrun: non-sep run
-    for nsrun in nsruns[:-1]:
-        if nsrun:
-            pre_lines.append(nsrun)
-        pre_lines.append(sep)
-    if nsruns[-1]:
-        pre_lines.append(nsruns[-1])
-    return pre_lines
 
 
 _SHORTEN_DIC = {
--- py/my_write_utils.py
+++ py/my_write_utils.py
@@ -4,13 +4,17 @@ Exports:
     bkg_path
 """
 
-import my_uni_heb as uh
+import my_html
 import my_open
+import my_uni_heb as uh
+import my_verse_and_friends as vaf
+import my_tanakh_book_names as tbn
+import my_str_defs as sd
 
 
 def write_bkg_in_un_fmt(variant, bkg_name, verses, rv_cant_that_covers):
     """ Write book group in "Unicode names" format. """
-    uh.do_quick_test()
+    do_quick_test_of_get_pre_lines()
     path = bkg_path(variant, bkg_name, fmt_is_unicode_names=True)
     title = f'unicode_names {bkg_name}'
     verses_dicts = {
@@ -54,4 +58,92 @@ def _write_callback(verses, rv_cant_that_covers, title, verses_dicts, out_fp):
             for roca in verses.keys()
             if verses_dicts[roca].get(bcvt) is not None
         }
-        uh.write_verse_un(out_fp, bcvt, multiverse)
+        _write_verse_un(out_fp, bcvt, multiverse)
+
+
+def _write_verse_un(out_fp, bcvt, multiverse):
+    """ Write verse in "unicode names" format """
+    bkid, chnu, vrnu = tbn.bcvt_get_bcv_triple(bcvt)
+    vtrad = tbn.bcvt_get_vtrad(bcvt)
+    out_fp.write(f'{bkid} {chnu}:{vrnu} in vtrad {vtrad}\n')
+    for rv_cant, body in multiverse.items():
+        if len(multiverse) > 1:
+            _write_segments(out_fp, body, rv_cant)
+        else:
+            _write_segments(out_fp, body, None)
+    out_fp.write('\n')
+
+
+def _write_segments(out_fp, some_kind_of_verse, cant_dab=None, indent=''):
+    if isinstance(some_kind_of_verse, (list, tuple)):
+        html_els = some_kind_of_verse
+        _write_segments_from_html_els(
+            out_fp, html_els, cant_dab, indent)
+        return
+    if isinstance(some_kind_of_verse, vaf.VerseAndFriends):
+        veraf = some_kind_of_verse
+        for html_els in (veraf.verse, veraf.next_cp, veraf.good_ending):
+            _write_segments_from_html_els(
+                out_fp, html_els, cant_dab, indent)
+        return
+    assert False, some_kind_of_verse
+
+
+def _write_segments_from_html_els(out_fp, html_els, cant_dab=None, indent=''):
+    if cant_dab:
+        out_fp.write(f'{cant_dab}\n')
+        indent = '    '
+    for html_el in html_els:
+        if isinstance(html_el, str):
+            pre_lines = [html_el]
+            for sep in ' ', sd.NBSP:
+                list_of_lists = [_get_pre_lines(sep, pl) for pl in pre_lines]
+                pre_lines = sum(list_of_lists, [])
+            for pre_line in pre_lines:
+                line = uh.comma_shunnas(pre_line)
+                out_fp.write(indent + line + '\n')
+            continue
+        if isinstance(html_el, dict):
+            segtag = my_html.hel_get_tag(html_el)  # e.g. 'span'
+            attr = html_el.get('attr')
+            kev_strs = _key_eq_val_strs(attr or {})
+            if contents := html_el.get('contents'):
+                out_fp.write(indent + _stasto('START', segtag, kev_strs))
+                _write_segments(out_fp, contents, None, indent)
+                out_fp.write(indent + _stasto('STOP', segtag, kev_strs))
+            else:
+                out_fp.write(indent + _stasto('START/STOP', segtag, kev_strs))
+            continue
+        assert False, 'instance of unexpected type'
+
+
+def _key_eq_val_strs(dic):
+    return tuple(f'{key}={val}' for key, val in dic.items())
+
+
+def _stasto(stasto, segtag, kev_strs):
+    return ' '.join((stasto, segtag, *kev_strs)) + '\n'
+
+
+def _get_pre_lines(sep, segment):
+    pre_lines = []
+    nsruns = segment.split(sep)  # nsrun: non-sep run
+    for nsrun in nsruns[:-1]:
+        if nsrun:
+            pre_lines.append(nsrun)
+        pre_lines.append(sep)
+    if nsruns[-1]:
+        pre_lines.append(nsruns[-1])
+    return pre_lines
+
+
+def do_quick_test_of_get_pre_lines():
+    """ Do a quick test. """
+    lines1 = _get_pre_lines(' ', ' '+'אבג')
+    lines2 = _get_pre_lines(' ', 'דהו'+' ')
+    lines3 = _get_pre_lines(' ', ' '+'זחט'+' ')
+    assert lines1 == [' ', 'אבג']
+    assert lines2 == ['דהו', ' ']
+    assert lines3 == [' ', 'זחט', ' ']
+
+
