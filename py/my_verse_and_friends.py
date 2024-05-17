2f55479d66b778357c520e09ae5a237e7ad67e50
Ben Denckla <bdenckla@alum.mit.edu>
2024-05-17 14:49:55 -0400

    add a couple of Python files that are needed
    my_verse_and_friends.py
    my_verse_and_friends.py

---
+++ py/my_verse_and_friends.py
@@ -0,0 +1,25 @@
+""" Exports VerseAndFriends """
+
+from dataclasses import dataclass
+
+@dataclass
+class VerseAndFriends:
+    """ Holds verse, maybe a next CP, & maybe a good ending. """
+    verse: tuple
+    vaf_next_cp: tuple
+    good_ending: tuple
+    def map_over(self, fun):
+        """
+        Make a new veraf by mapping fun over the fields of this veraf.
+        """
+        if isinstance(fun, tuple):
+            return VerseAndFriends(
+                fun[0](*fun[1:], self.verse),
+                fun[0](*fun[1:], self.vaf_next_cp),
+                fun[0](*fun[1:], self.good_ending),
+            )
+        return VerseAndFriends(
+            fun(self.verse),
+            fun(self.vaf_next_cp),
+            fun(self.good_ending),
+        )
