c27225e219075d157de742622017042f60bbc0c4
Ben Denckla <bdenckla@alum.mit.edu>
2025-10-01 09:54:16 -0400

    pysefaria phase 3

---
+++ py-example/pysefaria/mam4sef_handlers.py
@@ -0,0 +1,218 @@
+""" Exports HANDLERS """
+
+import py.my_html as my_html
+import pycmn.str_defs as sd
+import pycmn.hebrew_punctuation as hpu
+import pycmn.shrink as shrink
+
+# etel: ElementTree element
+# ofc1: output for all children, summed together
+# ofc2: output for all children, per child
+
+
+def _verse(etel, ofc1, _ofc2):
+    return ofc1 + _maybe_sampe(etel)
+
+
+def _text(etel, _ofc1, _ofc2):
+    return [etel.attrib["text"]]
+
+
+def _samekh2_or_3(_etel, _ofc1, _ofc2):
+    span = my_html.span_c(("{ס}",), "mam-spi-samekh")
+    return [sd.NBSP, span, sd.OCTO_NBSP]
+
+
+def _pe2_or_3(_etel, _ofc1, _ofc2):
+    span = my_html.span_c(("{פ}",), "mam-spi-pe")
+    return [sd.NBSP, span, my_html.line_break()]
+
+
+def _samekh3_nin(_etel, _ofc1, _ofc2):
+    """Handle a samekh3 element with class "nu10-invnun-neighbor" """
+    return [sd.NBSP]
+
+
+def _invnun(etel, _ofc1, _ofc2):
+    """
+    Handle either of the following two types of invnun elements:
+
+        Type 1: a default (i.e., no trailing space) invnun element.
+        These are the two Numbers 10 invnuns,
+        at the start of verse 35 and the end of verse 36.
+
+        Type 2: an invnun element with class "including-trailing-space"
+        These are the 7 Psalm 107 invnuns,
+        at the start of verses 23-28 and 40.
+    """
+    maybe_nbsp_dic = {"including-trailing-space": [sd.NBSP], None: []}
+    maybe_nbsp = maybe_nbsp_dic[etel.attrib.get("class")]
+    span = my_html.span_c((hpu.NUN_HAF,), "mam-spi-invnun")
+    return [span, *maybe_nbsp]
+
+
+def _legarmeih(_etel, _ofc1, _ofc2):
+    return [sd.THSP, my_html.bold((hpu.PASOLEG,))]
+
+
+def _paseq(_etel, _ofc1, _ofc2):
+    return [sd.THSP, my_html.small((hpu.PASOLEG,)), sd.THSP]
+
+
+def _empty(_etel, _ofc1, _ofc2):
+    return []
+
+
+def _pass_thru(_etel, ofc1, _ofc2):
+    return ofc1
+
+
+def _letter_small(_etel, ofc1, _ofc2):
+    return [my_html.small(ofc1)]
+
+
+def _letter_large(_etel, ofc1, _ofc2):
+    return [my_html.big(ofc1)]
+
+
+def _letter_hung(_etel, ofc1, _ofc2):
+    return [my_html.sup(ofc1)]
+
+
+def _kq_trivial(_etel, ofc1, _ofc2):
+    """Handle a trivial ketiv/qere element"""
+    return [my_html.span_c(ofc1, "mam-kq-trivial")]
+
+
+def _ketiv_qere(etel, _ofc1, ofc2):
+    sep_dic = {"sep-maqaf": hpu.MAQ, None: " "}
+    separator = sep_dic[etel.attrib.get("class")]
+    k_or_q, q_or_k = ofc2.values()
+    inside = [*k_or_q, separator, *q_or_k]
+    return [my_html.span_c(inside, "mam-kq")]
+
+
+def _ketiv(etel, ofc1, _ofc2):
+    """
+    Handle a ketiv element that is:
+       * the ketiv part of a ketiv ve qere (common)
+       * a ketiv velo qere (rare)
+    """
+    return _ketiv_or_qere_helper("mam-kq-k", "()", ofc1)
+
+
+def _k_velo_q_maq(_etel, _ofc1, _ofc2):
+    """
+    Handle the rare-within-rare (2 cases) of maqaf after ketiv velo qere.
+    """
+    return [my_html.span(hpu.MAQ, {"class": "k-velo-q-maq"})]
+
+
+def _qere(_etel, ofc1, _ofc2):
+    """
+    Handle a qere element that is:
+       * the qere part of a ketiv ve qere (common)
+       * a qere velo ketiv (rare)
+    """
+    return _ketiv_or_qere_helper("mam-kq-q", "[]", ofc1)
+
+
+def _scrdfftar(etel, _ofc1, ofc2):
+    target, _note = ofc2.values()
+    # starpos = etel.attrib["sdt-starpos"]
+    # assert starpos in ("before-word", "after-word")
+    # maybe_note_0 = note if starpos == "before-word" else []
+    # maybe_note_1 = note if starpos == "after-word" else []
+    # return maybe_note_0 + target + maybe_note_1
+    return target
+
+
+def _scrdfftar_target(_etel, ofc1, _ofc2):
+    return ofc1
+
+
+# def _scrdfftar_note(_etel, ofc1, _ofc2):
+#     """Handle a scroll difference note element"""
+#     paren_ofc1 = _paren(ofc1)
+#     el_sup = my_html.sup(["*"], {"class": "footnote-marker"})
+#     el_italic = my_html.italic(paren_ofc1, {"class": "footnote"})
+#     return [el_sup, el_italic]
+
+
+def _shirah_space(_etel, _ofc1, _ofc2):
+    return [sd.OCTO_NBSP]
+
+
+def _implicit_maqaf(_etel, _ofc1, _ofc2):
+    return [my_html.span_c([hpu.MAQ], "mam-implicit-maqaf")]
+
+
+#######################################################################
+#######################################################################
+
+
+def _paren(lst: list):
+    return shrink.shrink(["("] + lst + [")"])
+
+
+def _ketiv_or_qere_helper(the_class, brackets, ofc1):
+    b0_ofc1_b1 = [brackets[0], *ofc1, brackets[1]]
+    contents = shrink.shrink(b0_ofc1_b1)
+    return [my_html.span(contents, {"class": the_class})]
+
+
+def _maybe_sampe(etel):
+    ews = etel.attrib.get("ends-with-sampe")
+    if ews is None:
+        return []
+    sampe_fn_dic = {
+        "samekh2": _samekh2_or_3,
+        "samekh3": _samekh2_or_3,
+        "pe2": _pe2_or_3,
+        "pe3": _pe2_or_3,
+    }
+    sampe_fn = sampe_fn_dic[ews]
+    return sampe_fn(None, None, None)
+
+
+HANDLERS = {
+    ("verse", None): _verse,
+    ("text", None): _text,
+    #
+    ("good-ending", None): _empty,
+    ("letter-small", None): _letter_small,
+    ("letter-large", None): _letter_large,
+    ("letter-hung", None): _letter_hung,
+    #
+    ("kq-k-velo-q", None): _ketiv,
+    ("kq-k-velo-q-maq", None): _k_velo_q_maq,
+    ("kq-q-velo-k", None): _qere,
+    ("kq", None): _ketiv_qere,
+    ("kq", "sep-maqaf"): _ketiv_qere,
+    ("kq-k", None): _ketiv,
+    ("kq-q", None): _qere,
+    ("kq-trivial", None): _kq_trivial,
+    #
+    ("cant-combined", None): _pass_thru,
+    ("cant-alef", None): _empty,
+    ("cant-bet", None): _empty,
+    ("cant-all-three", None): _pass_thru,
+    #
+    ("spi-samekh2", None): _samekh2_or_3,
+    ("spi-samekh3", None): _samekh2_or_3,
+    ("spi-samekh3", "nu10-invnun-neighbor"): _samekh3_nin,
+    ("spi-pe2", None): _pe2_or_3,
+    ("spi-pe3", None): _pe2_or_3,
+    ("spi-invnun", None): _invnun,
+    ("spi-invnun", "including-trailing-space"): _invnun,
+    ("shirah-space", None): _shirah_space,
+    ("lp-legarmeih", None): _legarmeih,
+    ("lp-paseq", None): _paseq,
+    ("implicit-maqaf", None): _implicit_maqaf,
+    #
+    ("scrdfftar", None): _scrdfftar,
+    ("sdt-target", None): _scrdfftar_target,
+    ("sdt-note", None): _empty,
+    #
+    ("slh-word", None): _pass_thru,
+}
---
+++ py-example/pysefaria/mam4sef_or_ajf.py
@@ -0,0 +1,115 @@
+""" Exports main_helper """
+
+import xml.etree.ElementTree as ET
+import py.my_utils_for_mainish as my_utils_fm
+import py.osis_book_abbrevs as osis_book_abbrevs
+import pysefaria.sef_cmn as sef_cmn
+import pycmn.bib_locales as tbn
+import py.write_utils as write_utils
+import pysefaria.write_utils_sef_or_ajf as write_utils_sef_or_ajf
+import pycmn.shrink as shrink
+
+
+def main_helper(variant):
+    """Create the Sefaria MAM or AJF MAM from the XML MAM."""
+    bkids = my_utils_fm.get_bk39_tuple_from_argparse()
+    bkgs = osis_book_abbrevs.bk24_bkgs(bkids)
+    for bkg in bkgs:
+        do_one_book_group(variant, bkg)
+
+
+def _handle(handlers, etel):  # etel: ElementTree element
+    ofc1_raw = []  # output for all children, summed together
+    ofc2 = {}  # output for all children, per child
+    for child in etel:
+        output_for_child = _handle(handlers, child)
+        ofc1_raw.extend(output_for_child)
+        ofc2[child] = output_for_child
+    ofc1 = shrink.shrink(ofc1_raw)
+    attr_text = etel.attrib.get("text")
+    if attr_text is not None:
+        assert not ofc1
+        ofc1 = [attr_text]
+    tag_and_class = etel.tag, etel.attrib.get("class")
+    handler = handlers[tag_and_class]
+    return shrink.shrink(handler(etel, ofc1, ofc2))
+
+
+def _read_book_group(variant, bkg_name):
+    vtrad = variant["variant-vtrad"]
+    xml_vtrad_xxx_dic = {
+        tbn.VT_BHS: "xml-vtrad-bhs",
+        tbn.VT_SEF: "xml-vtrad-sef",
+    }
+    xml_vtrad_xxx = xml_vtrad_xxx_dic[vtrad]
+    xml_path = f"../MAM-XML/out/{xml_vtrad_xxx}/{bkg_name}.xml"
+    tree = ET.parse(xml_path)
+    return tree.getroot()
+
+
+XPATH_QUERY_FROM_CANT_DAB = {
+    # For cant_dual, we just look for all verses.
+    "rv-cant-combined": ".//verse",
+    # For cant_alef and cant_bet, we look for all verses that have a
+    # "cant-all-three" child.
+    "rv-cant-alef": ".//verse/cant-all-three/..",
+    "rv-cant-bet": ".//verse/cant-all-three/..",
+}
+
+
+def _process_book_group(variant, root, cant_dab):
+    handlers = variant["variant-handlers"]
+    vtrad = variant["variant-vtrad"]
+    if tuple(handlers.keys()) == _ALL_3_CANT_DAB_VALUES:
+        handlers2 = handlers[cant_dab]
+    else:
+        handlers2 = handlers
+    verses_in = root.findall(XPATH_QUERY_FROM_CANT_DAB[cant_dab])
+    bk39s_out = {}
+    for verse in verses_in:
+        osis_id = verse.attrib["osisID"]
+        bcvt = _get_bcvt_from_osis_id(vtrad, osis_id)
+        bkid = tbn.bcvt_get_bk39id(bcvt)
+        verse_out = _handle(handlers2, verse)
+        if bkid not in bk39s_out:
+            bk39s_out[bkid] = []
+        bk39s_out[bkid].append((bcvt, verse_out))
+    return bk39s_out
+
+
+def _get_bcvt_from_osis_id(vtrad, osid_id):
+    bkid, chnu, vrnu = osis_book_abbrevs.get_bcv_from_osis_id(osid_id)
+    return tbn.mk_bcvtxxx(bkid, chnu, vrnu, vtrad)
+
+
+def _do_for_cant_dab(bkg_out, variant, root, cant_dab):
+    bk39s = _process_book_group(variant, root, cant_dab)
+    for bkid, verses in bk39s.items():
+        if bkid not in bkg_out:
+            bkg_out[bkid] = {}
+        bkg_out[bkid][cant_dab] = verses
+
+
+def do_one_book_group(variant, bkg):
+    """Do the book group bkg"""
+    bkg_name = bkg["bkg-name"]
+    root = _read_book_group(variant, bkg_name)
+    bkg_out = {}
+    if variant.get("variant-include-abcants"):
+        cant_dabs = _ALL_3_CANT_DAB_VALUES
+    else:
+        cant_dabs = ("rv-cant-combined",)
+    for cant_dab in cant_dabs:
+        _do_for_cant_dab(bkg_out, variant, root, cant_dab)
+    for bkid, cant_to_verses in bkg_out.items():
+        sef_bkna = sef_cmn.SEF_BKNA[bkid]
+        csv_path = write_utils.bkg_path(variant, sef_bkna)
+        write_utils_sef_or_ajf.write_bkg_in_csv_fmt(
+            csv_path, variant, cant_to_verses, cant_dabs
+        )
+        write_utils.write_bkg_in_un_fmt(
+            variant, sef_bkna, cant_to_verses, "rv-cant-combined"
+        )
+
+
+_ALL_3_CANT_DAB_VALUES = "rv-cant-combined", "rv-cant-alef", "rv-cant-bet"
